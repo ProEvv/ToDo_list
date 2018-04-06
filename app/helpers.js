53742026b0329d51624bfcdac48a3f5f2058edd7
proevv <proevv1@yandex.ru>
2018-04-06 23:37:22 +0400

    maked class View and helpers

--- app/helpers.js
+++ app/helpers.js
@@ -1,1 +1,44 @@
 console.log('helpers.js');
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+    on(type, callback) {
+        this.events[type] = this.events[type] || [];
+        this.events[type].push(callback);
+    }
+    emit(type, arg) {
+        if (this.events[type]) {
+            this.events[type].array.forEach(callback => callback(arg));
+        } else {
+            console.log(`listener ${type} is undefined`)
+        }
+    }
+}
+
+function createElement(tag, props, ...children) {
+    const element = document.createElement(tag);
+
+    Object.keys(props).forEach(key => {
+        if (key.startsWith('data-')) {
+            element.setAttribute(key, props[key]);
+        } else {
+            element[key] = props[key];
+        }
+    });
+
+    children.forEach(child => {
+        if (typeof child === 'string') {
+            child = document.createTextNode(child);
+        }
+
+        element.appendChild(child);
+    });
+
+    return element;
+}
+
+export {
+    EventEmitter,
+    createElement
+};
